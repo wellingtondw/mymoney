8518209efbd488f0faec17fca1ee9452bb740ebb
wellington <wellingtonslpbi@hotmail.com>
2020-03-22 18:37:40 -0300

    useGet hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import useGet from './useGet'
+
+const url = 'https://mymoney-wls.firebaseio.com/moves.json'
 
 function App() {
+  const data = useGet(url)  
+
+  if(data.loading) {
+    return <p>Loading...</p>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My money</h1>
+      {JSON.stringify(data)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
